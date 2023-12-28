49cd8e65f88d4a2955c925cd3eb59493b0c7dc96
Matheus <matheus.mleall@gmail.com>
2023-12-28 15:33:40 -0300

    crated a the reset password functionality

--- src/api.js
+++ src/api.js
@@ -109,8 +109,28 @@ export function PHOTO_DELETE(id) {
 	};
 }
 
-// export function PHOTOS_GET(id) {
-// 	return {
-// 		url: `${API_URL}/api/photo/${id}`,
-// 	};
-// }
+export function PASSWORD_LOST(body) {
+	return {
+		url: `${API_URL}/api/password/lost`,
+		options: {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(body),
+		},
+	};
+}
+
+export function PASSWORD_RESET(body) {
+	return {
+		url: `${API_URL}/api/password/reset`,
+		options: {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(body),
+		},
+	};
+}
